535d1cb8bb95dd846edccbe20ef92f0e21215d0a
Hanna Andersson <hannaa78@gmail.com>
2016-06-10 10:47:37 +0200

    update controllers

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -24,19 +24,22 @@ angular.module('app.controllers', [])
 
 })
          
-.controller('loginCtrl', function($scope) {
+.controller('loginCtrl', ['$scope', '$firebaseObject', '$firebaseArray', 'Auth', '$state',
+  function($scope, $firebaseObject, $firebaseArray, Auth, $state) {
 
-})
+
+}])
 
 .controller('signupCtrl', ['$scope', '$firebaseObject', '$firebaseArray', 'Auth', '$state',
-  function($scope, $firebaseObject, $firebaseArray, Auth) {
+  function($scope, $firebaseObject, $firebaseArray, Auth, $state) {
 
     $scope.signUp = function (){
       Auth.$createUserWithEmailAndPassword($scope.user.email, $scope.user.password)
       .then(function(firebaseUser){
         console.log("User created with uid:", firebaseUser.uid);
+        console.log("redirect user to login page");
 
-
+        //här skickas man till login sidan när man registrerat ett konto
         $state.go('login');
       }).catch(function(error){
         console.log("oh no you have a error:", error);
